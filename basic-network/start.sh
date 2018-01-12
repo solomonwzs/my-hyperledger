e2cc4ec5e51ed64f5dba20b50d260efc324b69d1
solomon <solomon.wzs@gmail.com>
2018-01-12 19:32:37 +0800

    modified:   basic-network/start.sh

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -23,9 +23,15 @@ msg "Start containers..."
 docker-compose -f "${DIR}/docker-compose.yaml" up -d \
     ca.example.com \
     orderer.example.com \
-    peer0.org1.example.com \
-    peer0.org2.example.com \
     couchdb
+
+docker-compose -f "${DIR}/docker-compose.yaml" up -d \
+    peer0.org1.example.com \
+    peer1.org1.example.com 
+
+docker-compose -f "${DIR}/docker-compose.yaml" up -d \
+    peer0.org2.example.com \
+    peer1.org2.example.com 
 
 sleep 10
 
@@ -54,12 +60,20 @@ function join_channel() {
 
 msg "Create channel..."
 create_channel peer0.org1.example.com
-
 msg "peer0.org1.example.com join channel..."
 join_channel peer0.org1.example.com
 
 msg "Fetch channel..."
-fetch_channel peer0.org2.example.com
+fetch_channel peer1.org1.example.com
+msg "peer1.org1.example.com join channel..."
+join_channel peer1.org1.example.com
 
-msg "peer0.org1.example.com join channel..."
+msg "Fetch channel..."
+fetch_channel peer0.org2.example.com
+msg "peer0.org2.example.com join channel..."
 join_channel peer0.org2.example.com
+
+msg "Fetch channel..."
+fetch_channel peer1.org2.example.com
+msg "peer1.org2.example.com join channel..."
+join_channel peer1.org2.example.com
